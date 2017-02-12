49e394b901584978fd3fb10d32e1148311a9f708
aaleman11 <aaleman11@gmail.com>
2017-02-11 20:50:12 -0800

    Update equation notes

--- BayesianMLPlayground.playground/Contents.swift
+++ BayesianMLPlayground.playground/Contents.swift
@@ -1,5 +1,33 @@
 //: Playground - noun: a place where people can play
 
+/*
+ Conditional Probability:
+ P(A|B) = P(A and B)/P(B)
+ Read as: The probability of A given B = Probability of A and B divided by the Probability of B
+ 
+ Bayes Rule:
+ P(A|B) = P(B|A) * P(A) / P(B)
+ 
+ P(theta|D) = P(D|theta) * P(theta) / P(data)
+ 
+ If events A and B are not independent, then the probability of the intersection of A and B (the probability that both events occur) is defined by
+ 
+ P(A and B) = P(A)*P(B|A)
+ P(B|A) = P(A and B)/P(A)
+ 
+ 1 pounds = 0.453592 kilograms
+ 
+ P(New Max|Old Max) = P(New Max and Old Max)/P(Old Max)
+ 
+ 
+ 3 x 10 @ 75% - Three sets of 10 reps at 75% of the 1RM
+ 8/80%, 6/85%, 6/85%, 4/90% - Eight reps at 80%, two sets of six reps at 85%, and four reps at 90% of the 1RM
+
+ 
+
+ 
+ */
+
 import UIKit
 
-var str = "Hello, playground"
+
